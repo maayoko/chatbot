b6fb127f714352b948739814300827e7b2eaec70
Ivan <maayoko@gmail.com>
2022-02-01 18:57:53 +0100

    feat(chat): display chat's home page

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http.request import HttpRequest
 
 # Create your views here.
+def index(request: HttpRequest):
+    return render(request, "chat/index.html")
